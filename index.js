9d65d210f65cd785424bc7433737c149f99f1269
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2025-05-10 23:58:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ export default function MindMapGPT() {
   const generateMindMap = async () => {
     const res = await fetch("/api/generate", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },  kk
+      headers: { "Content-Type": "application/json" },  
       body: JSON.stringify({ topic })
     });
     const data = await res.json();
